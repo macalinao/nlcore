d7d4955b07ff00010e9df7ec60e84ebc7dec078f
Ian Macalinao <ianmacalinao@gmail.com>
2013-07-05 10:01:23 -0500

    Debug stuff
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/enderchestprotect/EnderChest.java
+++ src/main/java/net/new_liberty/enderchestprotect/EnderChest.java
@@ -97,6 +97,11 @@ public class EnderChest {
      * @return
      */
     public boolean isExpired() {
+        System.out.println("Expiry: " + getExpiryTime().getTime());
+        System.out.println("Now: " + System.currentTimeMillis());
+        System.out.println("Diff: " + (getExpiryTime().getTime() - System.currentTimeMillis()));
+        System.out.println("Pretty: " + DATE_FORMAT.format(getExpiryTime()));
+        System.out.println("Pretty2: " + DATE_FORMAT.format(new Timestamp(System.currentTimeMillis())));
         return (getExpiryTime().getTime() - System.currentTimeMillis()) <= 0;
     }
 
