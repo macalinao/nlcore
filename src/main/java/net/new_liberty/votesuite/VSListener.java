485b9edc23ff1f8eb7a36fa7ad18e040ccef77da
Ian Macalinao <ianmacalinao@gmail.com>
2013-07-07 07:25:18 -0500

    Store votes in database
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/votesuite/VSListener.java
+++ src/main/java/net/new_liberty/votesuite/VSListener.java
@@ -1,6 +1,10 @@
 package net.new_liberty.votesuite;
 
+import com.simplyian.easydb.EasyDB;
+import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -16,5 +20,17 @@ public class VSListener implements Listener {
 
     @EventHandler
     public void onVotifier(VotifierEvent event) {
+        Vote vote = event.getVote();
+
+        String name = vote.getUsername();
+        Player player = Bukkit.getPlayerExact(name);
+        if (player != null) { // Prefer the online name
+            name = player.getName();
+        }
+
+        // Timestamp has a different format for each voting website, so we are
+        // using our own which depends on the time it was received
+        EasyDB.getDb().update("INSERT INTO votes (name, service, address) VALUES (?, ?, ?) ",
+                name, vote.getServiceName(), vote.getAddress());
     }
 }
--- src/main/java/net/new_liberty/votesuite/VoteSuite.java
+++ src/main/java/net/new_liberty/votesuite/VoteSuite.java
@@ -11,6 +11,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class VoteSuite extends JavaPlugin {
     @Override
     public void onEnable() {
+        if (!EasyDB.getDb().isValid()) {
+            getLogger().log(Level.WARNING, "EasyDB isn't connected; plugin loading halted.");
+            return;
+        }
+
+        EasyDB.getDb().update("CREATE TABLE IF NOT EXISTS votes ("
+                + "id INT(10) NOT NULL AUTO_INCREMENT,"
+                + "name varchar(16) NOT NULL,"
+                + "service varchar(50) NOT NULL,"
+                + "address varchar(50) NOT NULL,"
+                + "time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"
+                + "PRIMARY KEY (id));");
+
         Bukkit.getPluginManager().registerEvents(new VSListener(this), this);
 
         getLogger().log(Level.INFO, "Plugin loaded.");
