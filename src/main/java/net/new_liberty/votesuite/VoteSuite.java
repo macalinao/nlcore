0e67f113be2b6c02002a241fe2aa7d24fffa6de1
Ian Macalinao <ianmacalinao@gmail.com>
2013-07-07 02:36:57 -0500

    Added listening to the Votifier event
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/votesuite/VoteSuite.java
+++ src/main/java/net/new_liberty/votesuite/VoteSuite.java
@@ -1,10 +1,18 @@
 package net.new_liberty.votesuite;
 
+import com.simplyian.easydb.EasyDB;
+import java.util.logging.Level;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * VoteSuite main class.
  */
 public class VoteSuite extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new VSListener(this), this);
 
+        getLogger().log(Level.INFO, "Plugin loaded.");
+    }
 }
