dc6789cc94f80968a3b06ee1e9920948f01255e4
Ian Macalinao <ianmacalinao@gmail.com>
2013-07-20 05:19:06 -0500

    Disable Ender Pearl damage
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/nltweaks/NLTweaks.java
+++ src/main/java/net/new_liberty/nltweaks/NLTweaks.java
@@ -1,22 +1,19 @@
 package net.new_liberty.nltweaks;
 
-import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionType;
 
 /**
  * This plugin contains all of the tweaks to New Liberty that don't warrant
@@ -36,6 +33,13 @@ public class NLTweaks extends JavaPlugin implements Listener {
             e.setCancelled(true);
             player.sendMessage(ChatColor.RED + "You can't use Ender Pearls on this server.");
             player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL));
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity2(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof EnderPearl) {
+            e.setCancelled(true);
         }
     }
 
