27658375ada154311f5b475326fe128018b2f441
Ian Macalinao <ianmacalinao@gmail.com>
2013-07-28 17:11:37 -0500

    Move colors to their own classes to enable testing
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/nltweaks/tweak/ChatColorCommands.java
+++ src/main/java/net/new_liberty/nltweaks/tweak/ChatColorCommands.java
@@ -28,83 +28,8 @@ public class ChatColorCommands extends Tweak {
 
     @Override
     public void onEnable() {
-        plugin.getCommand("setcolors").setExecutor(new ChatColorCommand("Colors", "chatcolorcommands.setcolors") {
-            @Override
-            public String validate(String rank, String prefix) {
-                int required = rank.length() + 1;
-                if (prefix.length() != required) {
-                    return "Your prefix needs to be " + required + " characters long.";
-                }
-
-                if (prefix.contains("5") || prefix.contains("0")) {
-                    return "Invalid prefix (violates restricted prefix colors)";
-                }
-
-                char lastCode = prefix.charAt(prefix.length() - 1);
-                if (equalsOne(lastCode, 'e', 'c', 'a', '5', 'd', '0')) {
-                    return "Invalid prefix (violates restricted name colors)";
-                }
-
-                char last = ' ';
-                for (int i = 0; i < prefix.length(); i++) {
-                    char cur = prefix.charAt(i);
-                    if (rsp.contains(last) && rsp.contains(cur)) {
-                        return "Invalid prefix (violates restricted solid prefix colors)";
-                    }
-
-                    last = cur;
-                }
-
-                return OK_RESPONSE;
-            }
-
-            @Override
-            public void execute(CommandSender sender, String rank, String value) {
-                StringBuilder b = new StringBuilder("[");
-                char[] colors = value.toCharArray();
-                char[] rankChars = rank.toCharArray();
-
-                for (int i = 0; i < rankChars.length; i++) {
-                    b.append('&').append(colors[i]).append(rankChars[i]);
-                }
-
-                String prefix = b.append("&f]&").append(colors[colors.length - 1]).toString();
-
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user "
-                        + sender.getName() + " prefix set " + prefix);
-                sender.sendMessage("Your colors have been set to: " + value);
-            }
-        });
-
-        plugin.getCommand("setchatcolor").setExecutor(new ChatColorCommand("Chat Color", "chatcolorcommands.setchatcolor") {
-            @Override
-            public String validate(String rank, String value) {
-                if (value.length() != 1) {
-                    return "Suffixes are only one colour!";
-                }
-
-                char suffix = value.charAt(0);
-                if (equalsOne(suffix, 'e', 'c', 'a', '5', 'd', '0', '4')) {
-                    return "Invalid suffix (violates restricted chat colors)";
-                }
-
-                ChatColor color = ChatColor.getByChar(suffix);
-                if (color == null) {
-                    return "Unknown color.";
-                }
-
-                return OK_RESPONSE;
-            }
-
-            @Override
-            public void execute(CommandSender sender, String rank, String value) {
-                ChatColor color = ChatColor.getByChar(value);
-
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user "
-                        + sender.getName() + " suffix set &" + color.getChar());
-                sender.sendMessage("Changed chat color to: " + value + "this color");
-            }
-        });
+        plugin.getCommand("setcolors").setExecutor(new SetColorsCommand());
+        plugin.getCommand("setchatcolor").setExecutor(new SetChatColorCommand());
     }
 
     /**
@@ -122,6 +47,9 @@ public class ChatColorCommands extends Tweak {
         return false;
     }
 
+    /**
+     * Represents a command that changes a chat color.
+     */
     public static abstract class ChatColorCommand implements CommandExecutor {
         private final String type;
 
@@ -174,5 +102,94 @@ public class ChatColorCommands extends Tweak {
         public abstract String validate(String rank, String value);
 
         public abstract void execute(CommandSender sender, String rank, String value);
+    }
+
+    public static class SetColorsCommand extends ChatColorCommand {
+        public SetColorsCommand() {
+            super("Colors", "chatcolorcommands.setcolors");
+        }
+
+        @Override
+        public String validate(String rank, String prefix) {
+            int required = rank.length() + 1;
+            if (prefix.length() != required) {
+                return "Your prefix needs to be " + required + " characters long.";
+            }
+
+            if (prefix.contains("5") || prefix.contains("0")) {
+                return "Invalid prefix (violates restricted prefix colors)";
+            }
+
+            char lastCode = prefix.charAt(prefix.length() - 1);
+            if (equalsOne(lastCode, 'e', 'c', 'a', '5', 'd', '0')) {
+                return "Invalid prefix (violates restricted name colors)";
+            }
+
+            char last = ' ';
+            for (int i = 0; i < prefix.length(); i++) {
+                char cur = prefix.charAt(i);
+                if (rsp.contains(last) && rsp.contains(cur)) {
+                    return "Invalid prefix (violates restricted solid prefix colors)";
+                }
+
+                last = cur;
+            }
+
+            return OK_RESPONSE;
+        }
+
+        @Override
+        public void execute(CommandSender sender, String rank, String value) {
+            StringBuilder b = new StringBuilder("[");
+            char[] colors = value.toCharArray();
+            char[] rankChars = rank.toCharArray();
+
+            for (int i = 0; i < rankChars.length; i++) {
+                b.append('&').append(colors[i]).append(rankChars[i]);
+            }
+
+            String prefix = b.append("&f]&").append(colors[colors.length - 1]).toString();
+
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user "
+                    + sender.getName() + " prefix set " + prefix);
+            sender.sendMessage("Your colors have been set to: " + value);
+        }
+    }
+
+    /**
+     * Command to set the chat color.
+     */
+    public static class SetChatColorCommand extends ChatColorCommand {
+        public SetChatColorCommand() {
+            super("Chat Color", "chatcolorcommands.setchatcolor");
+        }
+
+        @Override
+        public String validate(String rank, String value) {
+            if (value.length() != 1) {
+                return "Suffixes are only one colour!";
+            }
+
+            char suffix = value.charAt(0);
+            if (equalsOne(suffix, 'e', 'c', 'a', '5', 'd', '0', '4')) {
+                return "Invalid suffix (violates restricted chat colors)";
+            }
+
+            ChatColor color = ChatColor.getByChar(suffix);
+            if (color == null) {
+                return "Unknown color.";
+            }
+
+            return OK_RESPONSE;
+        }
+
+        @Override
+        public void execute(CommandSender sender, String rank, String value) {
+            ChatColor color = ChatColor.getByChar(value);
+
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user "
+                    + sender.getName() + " suffix set &" + color.getChar());
+            sender.sendMessage("Changed chat color to: " + value + "this color");
+        }
     }
 }
