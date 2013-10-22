cb685c9de54ebc53d03edde4a3f239d459444987
Ian Macalinao <ianmacalinao@gmail.com>
2013-10-21 23:59:16 -0500

    Remove unnecessary import.
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/nltweaks/tweak/specialeggs/eggs/FreezeEgg.java
+++ src/main/java/net/new_liberty/nltweaks/tweak/specialeggs/eggs/FreezeEgg.java
@@ -17,7 +17,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 /**
