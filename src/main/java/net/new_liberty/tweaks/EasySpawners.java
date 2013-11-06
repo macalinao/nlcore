a5d54a3d8e4584d1d6584d58b670df0a514451c3
Ian Macalinao <ianmacalinao@gmail.com>
2013-11-06 17:23:11 -0600

    Drop spawners from certain mobs that spawned naturally with a 1% chance.
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/tweaks/EasySpawners.java
+++ src/main/java/net/new_liberty/tweaks/EasySpawners.java
@@ -1,7 +1,11 @@
 package net.new_liberty.tweaks;
 
+import java.util.List;
+import java.util.Random;
+import net.new_liberty.nlcore.NLCore;
 import net.new_liberty.nlcore.module.Module;
 import org.apache.commons.lang.WordUtils;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
@@ -9,8 +13,13 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 
 /**
  * Makes spawners have different mobs assigned to them. These spawners are
@@ -18,6 +27,10 @@ import org.bukkit.inventory.meta.ItemMeta;
  * dropped by mobs.
  */
 public class EasySpawners extends Module {
+
+    public static final double DROP_CHANCE = 0.01;
+
+    private Random r = new Random();
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
@@ -51,12 +64,54 @@ public class EasySpawners extends Module {
         CreatureSpawner s = (CreatureSpawner) e.getBlock().getState();
         EntityType t = s.getSpawnedType();
 
+        dropSpawner(t, e.getBlock().getLocation());
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if (e.getSpawnReason() == SpawnReason.NATURAL) {
+            e.getEntity().setMetadata("n", new FixedMetadataValue(NLCore.i(), true));
+        }
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e) {
+        switch (e.getEntityType()) {
+            case CREEPER:
+            case SKELETON:
+            case SPIDER:
+            case ZOMBIE:
+            case SLIME:
+            case CAVE_SPIDER:
+            case BAT:
+            case PIG:
+            case SHEEP:
+            case COW:
+            case CHICKEN:
+            case OCELOT:
+                break;
+            default: // Anything not allowed
+                return;
+        }
+
+        List<MetadataValue> vl = e.getEntity().getMetadata("n");
+        for (MetadataValue m : vl) {
+            if (m.getOwningPlugin().equals(NLCore.i()) && m.asBoolean()) {
+                // yes, this is natural
+                if (r.nextDouble() < DROP_CHANCE) {
+                    dropSpawner(e.getEntityType(), e.getEntity().getLocation());
+                }
+                return;
+            }
+        }
+    }
+
+    private void dropSpawner(EntityType e, Location l) {
         ItemStack i = new ItemStack(Material.MOB_SPAWNER);
         ItemMeta m = i.getItemMeta();
-        m.setDisplayName(WordUtils.capitalize(t.name()).replace('_', ' '));
+        m.setDisplayName(WordUtils.capitalize(e.name()).replace('_', ' '));
         i.setItemMeta(m);
-
-        e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), i);
+        l.getWorld().dropItemNaturally(l, i);
     }
 
 }
