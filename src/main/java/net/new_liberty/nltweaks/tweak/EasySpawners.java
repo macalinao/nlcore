2e3f88b22ea6a681f35fd78347081980fcb0ecab
Ian Macalinao <ianmacalinao@gmail.com>
2013-11-10 19:06:40 -0600

    Fix bug where players could rename spawnerS
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/nltweaks/tweak/EasySpawners.java
+++ src/main/java/net/new_liberty/nltweaks/tweak/EasySpawners.java
@@ -1,18 +1,25 @@
 package net.new_liberty.nltweaks.tweak;
 
 import net.new_liberty.nltweaks.Tweak;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 /**
  * Makes spawners easy.
  */
 public class EasySpawners extends Tweak {
+
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Block b = event.getBlock();
@@ -35,4 +42,30 @@ public class EasySpawners extends Tweak {
         CreatureSpawner s = (CreatureSpawner) b.getState();
         s.setSpawnedType(t);
     }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (!(e.getWhoClicked() instanceof Player)) {
+            return;
+        }
+
+        if (e.getInventory().getType() != InventoryType.ANVIL) {
+            return;
+        }
+
+        if (e.getSlotType() != SlotType.RESULT || e.getSlot() != 2) {
+            return;
+        }
+
+        ItemStack i = e.getCurrentItem();
+        if (i == null || i.getType() != Material.MOB_SPAWNER) {
+            return;
+        }
+
+        Player p = (Player) e.getWhoClicked();
+        p.sendMessage(ChatColor.RED + "You can't rename mob spawners.");
+        e.setCancelled(true);
+        p.closeInventory();
+    }
+
 }
