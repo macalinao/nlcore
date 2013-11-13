add220f5f1f6223ec1923a06d43e6fb1e0fa74fb
Ian Macalinao <ianmacalinao@gmail.com>
2013-11-13 17:03:26 -0600

    Add database code to EnderChestBank
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/enderchestbank/EnderChestBank.java
+++ src/main/java/net/new_liberty/enderchestbank/EnderChestBank.java
@@ -4,10 +4,25 @@
  */
 package net.new_liberty.enderchestbank;
 
+import com.simplyian.easydb.EasyDB;
 import net.new_liberty.nlcore.module.Module;
 
 /**
  * Handles the single big ender chest at spawn.
  */
 public class EnderChestBank extends Module {
+
+    @Override
+    public String[] getDependencies() {
+        return new String[]{"EasyDB"};
+    }
+
+    @Override
+    public void onEnable() {
+        EasyDB.getDb().update("CREATE TABLE IF NOT EXISTS ecb_inventories ("
+                + "owner VARCHAR(16) NOT NULL,"
+                + "contents TEXT,"
+                + "PRIMARY KEY (owner));");
+    }
+
 }
