0358e77586f7b1f6ee14baf6594a0c1e66835f46
Ian Macalinao <ianmacalinao@gmail.com>
2013-11-13 19:41:07 -0600

    Restrict bank chest placement to admins
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

---
+++ src/main/java/net/new_liberty/bankchests/BCListener.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.new_liberty.bankchests;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+/**
+ * Bank chest listener
+ */
+public class BCListener implements Listener {
+
+    private final BankChests b;
+
+    public BCListener(BankChests b) {
+        this.b = b;
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if (e.getBlock().getType() != Material.ENDER_CHEST) {
+            return;
+        }
+
+        Player p = e.getPlayer();
+        if (!p.hasPermission("bankchests.admin")) {
+            p.sendMessage(ChatColor.RED + "You're not allowed to place Ender Chests.");
+            e.setCancelled(true);
+            return;
+        }
+
+        p.sendMessage(ChatColor.YELLOW + "You have placed an ender chest (bank chest).");
+    }
+
+}
--- src/main/java/net/new_liberty/bankchests/BankChests.java
+++ src/main/java/net/new_liberty/bankchests/BankChests.java
@@ -12,6 +12,8 @@ import net.new_liberty.nlcore.module.Module;
  */
 public class BankChests extends Module {
 
+    private BCListener l;
+
     @Override
     public String[] getDependencies() {
         return new String[]{"EasyDB"};
@@ -19,10 +21,15 @@ public class BankChests extends Module {
 
     @Override
     public void onEnable() {
+        l = new BCListener(this);
+        addListener(l);
+
         EasyDB.getDb().update("CREATE TABLE IF NOT EXISTS ecb_inventories ("
                 + "owner VARCHAR(16) NOT NULL,"
                 + "contents TEXT,"
                 + "PRIMARY KEY (owner));");
+
+        addPermission("bankchests.admin", "Allows placement of ender chests.");
     }
 
 }
